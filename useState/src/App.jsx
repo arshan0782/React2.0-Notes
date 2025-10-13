0cfa9eb0df2f1dd0ba1ec42cb4c62912f8f31e59
arshan0782 <arshan0782@gmail.com>
2025-10-13 17:11:07 +0530

    first commit

---
+++ useState/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [num, setNum] = useState(10)
+
+
+    function updateNum(){
+        setNum(num + 1)
+    }
+    const changeDec =()=> setNum(num - 1)
+
+  return (
+   <div>
+     <h1>Value of a is {num}</h1>
+    <button onClick={updateNum}> Increase</button>
+    <button onClick={changeDec}> Decrease</button>
+    <button onClick={()=> setNum(num * 2)}> Double</button>
+    <button onClick={()=> setNum(0)}> Reset</button>
+   </div>
+  )
+}
+
+export default App
