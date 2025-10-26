250ac363681643a7d42c4b9769973c8d6702a989
arshan0782 <arshan0782@gmail.com>
2025-10-26 20:09:21 +0530

    nestedRouting & Dynamic

--- 11-advaced-routing/src/pages/About.jsx
+++ 11-advaced-routing/src/pages/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 const About = () => {
+  let navigate = useNavigate()
+
+  const  btnCliked =()=>{
+   navigate('/')
+    
+  }
+
   return (
-    <div>
+    <div className='about-content'>
+      <button onClick={btnCliked} className='btn'>Go Home Page</button>
         <h1>About Page</h1>
     </div>
   )
--- 11-advaced-routing/src/pages/ArshanDetails.jsx
+++ 11-advaced-routing/src/pages/ArshanDetails.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 
 const ArshanDetails = () => {
+
+  const params = useParams() 
+
+console.log(params);
   return (
     <div className="details-container">
       <div className="card">
